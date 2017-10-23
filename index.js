c84fa05d4e493e3f3c6fb4f7d4d9fa120ce32bda
Annette Michaels <micha232@umn.edu>
2017-10-23 20:15:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,11 +86,10 @@ function createRock(x) {
      * we should call endGame()
      */
      var rocktop = positionToInteger(rock.style.top)
+     debugger
      if (rocktop < 380) {
        rock.style.top = `${rocktop + 2}px`
      } else if (rocktop >= 380) {
-        debugger
-        console.log("removed")
         rock.remove()
       }
      if (checkCollision(rock)) {
