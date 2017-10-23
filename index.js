558184fafd65fc1d31b38cdc8e3bc96b2df4878a
Annette Michaels <micha232@umn.edu>
2017-10-23 21:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ function createRock(x) {
        rocktop = rocktop + 2
      } if (rocktop >= 400) {
         rock.remove()
+      ROCKS.pop()
         return
     } else if (checkCollision(rock)) {
        endGame()
